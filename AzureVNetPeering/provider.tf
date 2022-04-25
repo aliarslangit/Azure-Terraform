bf8a16eaef8b114cbf8fd5d5ff48edea763a7bae
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-04-25 17:56:15 +0500

    Create provider.tf

---
+++ AzureVNetPeering/provider.tf
@@ -0,0 +1,26 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+
+  }
+}
+
+# Provider Block
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+
+
+}
+
+
+
