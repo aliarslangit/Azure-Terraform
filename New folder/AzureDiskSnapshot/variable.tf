9290743e6240a7931d43eab6a6da789b145d211a
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-05-13 19:31:35 +0500

    Create variable.tf

---
+++ New folder/AzureDiskSnapshot/variable.tf
@@ -0,0 +1,49 @@
+//-------------------------------Service principal------------------------------//
+
+variable "azure_subscription_id" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "azure_client_id" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "azure_client_secret" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "azure_tenant_id" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+//------------------------------------TAGS-----------------------------------------\\
+variable "owner" {
+  type        = string
+  default     = "Ashwani"
+  description = "description"
+}
+
+variable "environment" {
+  type    = string
+  default = "Dev"
+}
+
+
+
+//------------------------CONFIGIRATIONS------------------------------------------//
+
+variable "rgname" {
+  default = "test_demorgwebapp"
+}
+
+variable "location" {
+  default = "West US"
+}
