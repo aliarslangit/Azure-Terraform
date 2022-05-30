4cac1fb2491eef99e65d437509c73996db1bb363
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-05-30 18:31:47 +0500

    Create variables.tf

---
+++ AzureBastion/variables.tf
@@ -0,0 +1,50 @@
+//-------------------------------Service principal------------------------------//
+
+variable "azure_subscription_id" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "azure_client_id" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "azure_client_secret" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "azure_tenant_id" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+//------------------------Bastion CONFIGIRATIONS------------------------------------------
+variable "rgname" {
+  default = "demorg_cdn"
+}
+
+variable "location" {
+  default = "West US"
+}
+
+variable "bastionhostname" {
+  default = "demobastion"
+}
+
+//------------------------------------TAGS-------------------------\\
+variable "owner" {
+  type        = string
+  default     = "Ali"
+  description = "description"
+}
+
+variable "environment" {
+  type    = string
+  default = "Dev"
+}
